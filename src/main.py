ec357a2e03fbdba5647494056a7b5539af3f311a
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2021-05-04 00:07:31 +0300

    Simplify and beatify

--- src/main.py
+++ src/main.py
@@ -5,6 +5,17 @@ from wrappers import make_env
 import torch
 from dqn import DQN
 from train import train_agent
+from gym.utils.play import play
+import numpy as np
+from gym.envs.classic_control import rendering
+
+
+def repeat_upsample(rgb_array, k=1, l=1, err=[]):
+    # repeat kinda crashes if k/l are zero
+    if k <= 0 or l <= 0:
+        return rgb_array
+
+    return np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)
 
 
 @click.group()
@@ -13,19 +24,25 @@ def cli():
 
 
 @cli.command()
-@click.option('--env_name', default='Breakout-v0',
+@click.option('--env_name', default='BreakoutDeterministic-v4',
               help='Name of the gym environment.')
 @click.option('--it_count', default=0,
               help='Bound number of ')
-@click.option('--delay', default=0.0,
+@click.option('--delay', default=0.1,
               help='Delay duration to set the game speed.')
-def random_agent(env_name, it_count, delay):
+@click.option('--zoom', default=3,
+              help='Adjusts the size of the game window.')
+def random_agent(env_name, it_count, delay, zoom):
+    viewer = rendering.SimpleImageViewer()
     env = gym.make(env_name)
     observation = env.reset()
 
     iteration = 0
     while iteration <= it_count:
-        env.render()
+        rgb = env.render('rgb_array')
+        upscaled = repeat_upsample(rgb, zoom, zoom)
+        viewer.imshow(upscaled)
+
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
@@ -37,90 +54,27 @@ def random_agent(env_name, it_count, delay):
 
 
 @cli.command()
-@click.option('--env_name', default='Breakout-v0',
+@click.option('--env_name', default='BreakoutDeterministic-v4',
               help='Name of the gym environment.')
-@click.option('--delay', default=0.1,
-              help='Delay duration to set the game speed.')
-def keyboard_agent(env_name, delay):
-    env = gym.make(env_name)
-    ACTIONS = env.action_space.n
-    SKIP_CONTROL = 0
+@click.option('--fps', default=30,
+              help='FPS to set the game speed.')
+@click.option('--zoom', default=3,
+              help='Adjusts the size of the game window.')
+def keyboard_agent(env_name, fps, zoom):
 
-    def key_press(key, mod):
-        global human_agent_action, \
-            human_wants_restart, \
-            human_sets_pause
-        if key == 0xff0d:
-            human_wants_restart = True
-        if key == 32:
-            human_sets_pause = not human_sets_pause
-        a = int(key - ord('0'))
-        if a <= 0 or a >= ACTIONS:
-            return
-        human_agent_action = a
-
-    def key_release(key, mod):
-        global human_agent_action
-        a = int(key - ord('0'))
-        if a <= 0 or a >= ACTIONS:
-            return
-        if human_agent_action == a:
-            human_agent_action = 0
-
-    env.render()
-    env.unwrapped.viewer.window.on_key_press = key_press
-    env.unwrapped.viewer.window.on_key_release = key_release
-
-    def rollout(env):
-        global human_agent_action, \
-            human_wants_restart, \
-            human_sets_pause
-        human_wants_restart = False
-        obser = env.reset()
-        skip = 0
-        total_reward = 0
-        total_timesteps = 0
-        while True:
-            if not skip:
-                a = human_agent_action
-                total_timesteps += 1
-                skip = SKIP_CONTROL
-            else:
-                skip -= 1
-
-            obser, r, done, info = env.step(a)
-            if r != 0:
-                print('reward %0.3f' % r)
-            total_reward += r
-            window_still_open = env.render()
-            if not window_still_open:
-                return False
-            if done or human_wants_restart:
-                break
-            while human_sets_pause:
-                env.render()
-                time.sleep(delay)
-            time.sleep(delay)
-        print((f'timesteps {total_timesteps} '
-               f'reward {total_reward:0.2f}'))
-
-    print(f'ACTIONS={ACTIONS}')
-    print('Press keys 1 2 3 ... to take actions 1 2 3 ...')
-    print('No keys pressed is taking action 0')
-
-    while True:
-        window_still_open = rollout(env)
-        if window_still_open == False:
-            break
+    play(gym.make(env_name), fps=fps, zoom=zoom)
 
 
 @cli.command()
 @click.argument('model')
-@click.option('--env_name', default='Breakout-v0',
+@click.option('--env_name', default='BreakoutDeterministic-v4',
               help='Name of the gym environment.')
-@click.option('--delay', default=0.02,
+@click.option('--delay', default=0.1,
               help='Delay duration to set the game speed.')
-def observe(model, env_name, delay):
+@click.option('--zoom', default=3,
+              help='Adjusts the size of the game window.')
+def observe(model, env_name, delay, zoom):
+    viewer = rendering.SimpleImageViewer()
     env = make_env(env_name)
     ACTION_COUNT = env.action_space.n
     dqn = torch.load(model).cpu()
@@ -138,7 +92,10 @@ def observe(model, env_name, delay):
 
             obs, rew, done, info = env.step(action)
             total_rew += rew
-            env.render()
+
+            rgb = env.render('rgb_array')
+            upscaled = repeat_upsample(rgb, zoom, zoom)
+            viewer.imshow(upscaled)
             time.sleep(delay)
 
             if done:
